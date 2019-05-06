c5fa4e7b4bc8d3a00710ace2ec7ab241d71d31dd
NoSym <rumplerump@gmail.com>
2019-05-06 03:21:09 -0400

    Add project files.

---
+++ WildMagic/MagicHandler.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using BepInEx;
+using RoR2;
+using RoR2.CharacterAI;
+using UnityEngine;
+using UnityEngine.Networking;
+
+// Wherein I begin using PascalCase method names
+namespace WildMagic
+{
+    /// <summary>
+    /// Controller for Wild Magic functionality. Methods are aimed at players but most will work with any CharacterMaster.
+    /// </summary>
+    public class MagicHandler
+    {
+        private CharacterMaster master;
+
+        /// <summary>
+        /// Creates a new wild magic controller for the specified character master.
+        /// </summary>
+        /// <param name="magicMaster">Owner of wild magic effects.</param>
+        public MagicHandler(CharacterMaster magicMaster)
+        {
+            master = magicMaster;
+        } // MagicHandler Constructor
+
+        /// <summary>
+        /// Roll a random available effect.
+        /// </summary>
+        public void Roll()
+        {
+
+        } // roll
+
+        // Yowch
+        private void DestroyEquipment()
+        {
+            master.inventory.SetEquipmentIndex(EquipmentIndex.None);
+        } // destroyEquipment
+
+        // Jackpot
+        private void DoubleMoney()
+        {
+            if (master.money < uint.MaxValue / 2)
+                master.money *= 2;
+        } // doubleMoney
+
+        // Make a gnome
+        private void Effigy()
+        {
+            NetworkServer.Spawn(UnityEngine.Object.Instantiate<GameObject>(Resources.Load<GameObject>("Prefabs/NetworkedObjects/CrippleWard"), master.GetBody().corePosition, Quaternion.identity));
+        } // effigy
+
+        // Sorry
+        private void HalveMoney()
+        {
+            master.money /= 2;
+        } // halveMoney
+
+        // Light em up
+        private void Hellfire()
+        {
+            master.GetBody().AddHelfireDuration(6f);
+        } // hellfire
+
+        // 1 meteor wave
+        private void Meteors()
+        {
+            MeteorStormController component = UnityEngine.Object.Instantiate<GameObject>(Resources.Load<GameObject>("Prefabs/NetworkedObjects/MeteorStorm"), master.GetBody().corePosition, Quaternion.identity).GetComponent<MeteorStormController>();
+            component.ownerDamage = master.GetBody().damage;
+            component.isCrit = Util.CheckRoll(master.GetBody().crit, master);
+            NetworkServer.Spawn(component.gameObject);
+        } // meteors
+
+        // Because why not
+        private void RandomizeSurvivor()
+        {
+            string[] survivorPrefabs = { "CommandoBody", "ToolbotBody", "HuntressBody", "EngiBody", "MageBody", "MercBody" };
+            string newSurvivor = survivorPrefabs[UnityEngine.Random.Range(0, survivorPrefabs.Length)];
+            GameObject newBody = BodyCatalog.FindBodyPrefab(newSurvivor);
+            master.bodyPrefab = newBody;
+            master.Respawn(master.GetBody().footPosition, master.GetBody().transform.rotation, true);
+        } // randomizeSurvivor
+
+        // A Friend
+        private void SpawnBeetleGuard()
+        {
+            GameObject prefab = MasterCatalog.FindMasterPrefab("BeetleGuardAlly" + "Master");
+            GameObject body = BodyCatalog.FindBodyPrefab("BeetleGuardAlly" + "Body");
+
+            GameObject beetle = UnityEngine.Object.Instantiate<GameObject>(prefab, master.GetBody().transform.position, Quaternion.identity);
+            beetle.AddComponent<MasterSuicideOnTimer>().lifeTimer = 300f; // Summons the boy for 5 minutes
+            CharacterMaster beetleMaster = beetle.GetComponent<CharacterMaster>();
+            beetle.GetComponent<BaseAI>().leader.gameObject = beetleMaster.GetBody().gameObject;
+            beetleMaster.teamIndex = TeamIndex.Player;
+
+            NetworkServer.Spawn(beetle);
+            beetleMaster.SpawnBody(body, master.GetBody().transform.position, Quaternion.identity);
+        } // spawnBeetleGuard
+
+        // Yes I'm writing a function for this
+        private void TakeOneDamage()
+        {
+            DamageInfo damageInfo = new DamageInfo();
+            damageInfo.damage = 1.0f;
+            damageInfo.damageType = DamageType.NonLethal;
+            damageInfo.damageColorIndex = DamageColorIndex.WeakPoint;
+            master.GetBody().healthComponent.TakeDamage(damageInfo);
+        } // takeOneDamage
+    } // MagicHandler Class
+} // Wildmagic Namespace
---
+++ WildMagic/WildMagic.cs
@@ -0,0 +1,317 @@
+﻿using System;
+using BepInEx;
+using RoR2;
+using RoR2.CharacterAI;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace WildMagic
+{
+    [BepInDependency("com.bepis.r2api")]
+    [BepInPlugin("com.NoSym.wildmagic", "WildMagic", "1.0.0")]
+    public class WildMagic : BaseUnityPlugin
+    {
+        private bool haunted = false;
+        private string goofName = "";
+        private float dmgBuff = 0;
+        private float dmgBuffTimer = -1;
+        private float dmgDebuff = 0;
+        private float dmgDebuffTimer = -1;
+        private float moveBuff = 0;
+        private float moveBuffTimer = -1;
+        private float hideTimer = -1;
+        private float trailTimer = -1;
+        private float hauntedTimer = -1;
+        private float funballTimer = -1;
+        private float ghostTimer = -1;
+        private float tankDamageBuff = 0;
+        private float tankMoveDebuff = 0;
+        private float tankArmorBuff = 0;
+        private float tankTimer = -1;
+
+        private DamageTrail[] trailArray = new DamageTrail[10];
+
+        // Woke
+        public void Awake()
+        {
+            // Not important Really
+            On.RoR2.CharacterBody.OnDamageDealt += (orig, self, report) =>
+            {
+                if (self.Equals(PlayerCharacterMasterController.instances[0].master.GetBody()))
+                    ;// instakill(report.victimMaster);// polymorph(report.victimMaster);
+
+                orig(self, report);
+            };
+
+            // Spooky
+            On.RoR2.GlobalEventManager.OnCharacterDeath += (orig, self, report) =>
+            {
+                // Ghosts
+                if (haunted && report.damageInfo.attacker.Equals(PlayerCharacterMasterController.instances[0].master.GetBody().gameObject))
+                {
+                    CharacterBody ghost = Util.TryToCreateGhost(report.victimBody, report.victimBody, 30);
+                    ghost.baseDamage /= 6.0f; // I mean seriously, 500%?
+                } // if
+
+                orig(self, report);
+            };
+
+            // They took my naaaame Juuustin
+            On.RoR2.PlayerCharacterMasterController.GetDisplayName += (orig, self) =>
+            {
+                if (self.Equals(PlayerCharacterMasterController.instances[0]) && goofName != "")
+                    return goofName;
+                else
+                    return orig(self);
+            };
+
+        } // Awake
+
+        // Spooky
+        private void beginHaunt()
+        {
+            haunted = true;
+            hauntedTimer = 1800; // 30 seconds
+        } // evilGhost
+
+        // Yeehaw
+        private void buffDamage()
+        {
+            if (dmgBuffTimer == -1)
+            {
+                PlayerCharacterMasterController p = PlayerCharacterMasterController.instances[0];
+                dmgBuff = p.master.GetBody().baseDamage;
+                p.master.GetBody().baseDamage += dmgBuff;
+                dmgBuffTimer = 900; // 15 seconds
+            } // if
+        } // buffDamage
+
+        // Go fast
+        private void buffMove()
+        {
+            if(moveBuffTimer == -1)
+            {
+                PlayerCharacterMasterController p = PlayerCharacterMasterController.instances[0];
+                moveBuff = p.master.GetBody().baseMoveSpeed;
+                p.master.GetBody().baseMoveSpeed += moveBuff;
+                moveBuffTimer = 900; // 15 seconds
+            } // if
+        } // buffMove
+
+        // 'Pardner
+        private void debuffDamage()
+        {
+            if (dmgDebuffTimer == -1)
+            {
+                PlayerCharacterMasterController p = PlayerCharacterMasterController.instances[0];
+                dmgDebuff = p.master.GetBody().baseDamage / 2;
+                p.master.GetBody().baseDamage -= dmgDebuff;
+                dmgDebuffTimer = 900; // 15 seconds
+            } // if
+        } // debuffDamage
+
+        // Let it burn
+        private void fireTrail(CharacterMaster master)
+        {
+            if (trailTimer == -1)
+            {
+                for (int i = 0; i < trailArray.Length; i++)
+                {
+                    DamageTrail wildFire;
+                    wildFire = Instantiate<GameObject>(Resources.Load<GameObject>("Prefabs/FireTrail"), master.GetBody().transform).GetComponent<DamageTrail>();
+                    wildFire.transform.position = master.GetBody().footPosition;
+                    wildFire.owner = master.GetBody().gameObject;
+                    wildFire.radius *= master.GetBody().radius * 10;
+                    wildFire.damagePerSecond *= master.GetBody().damage * 1.5f;
+                    trailArray[i] = wildFire;
+                } // for
+                trailTimer = 3600; // 1 minute lifespan
+            } // if
+        } // fireTrail
+
+        // Just activates the artifact
+        private void funballs()
+        {
+            if (!Run.instance.enabledArtifacts.HasArtifact(ArtifactIndex.Bomb))
+            {
+                Run.instance.enabledArtifacts.AddArtifact(ArtifactIndex.Bomb);
+                funballTimer = 3600; // 1 minute
+            } // if
+        } // funballs
+
+        // Catch em all
+        private void goGhost()
+        {
+            if (ghostTimer == -1)
+            {
+                PlayerCharacterMasterController.instances[0].master.inventory.GiveItem(ItemIndex.Ghost, 1);
+                ghostTimer = 600; // 10 seconds
+            } // if
+        } // goGhost
+
+        // Mildly inconvenient
+        private void hideCrosshair()
+        {
+            PlayerCharacterMasterController.instances[0].master.GetBody().hideCrosshair = true;
+            hideTimer = 3600; // 1 minute
+        } // hideCrosshair
+
+        // Boom
+        private void instakill(CharacterMaster victim)
+        {
+            victim.GetBody().healthComponent.Suicide();
+        } // instakill
+
+        // Note that this keeps the original ai, which is kinda funny since it's literally the previous enemy's brain in a new body
+        // THIS DOES NOT WORK RIGHT NOW
+        private void polymorph(CharacterMaster master)
+        {
+            string[] names = { "Beetle", "Bell", "Bison", "Golem", "GreaterWisp", "HermitCrab", "Imp", "Jellyfish", "LemurianBruiser", "Lemurian", "Wisp" };
+            string name = names[(int)UnityEngine.Random.Range(0, names.Length)];
+            master.bodyPrefab = BodyCatalog.FindBodyPrefab(name + "Body");
+            master.Respawn(master.GetBody().footPosition, master.GetBody().transform.rotation, true);
+            // Atm the guy will spawn and just stand there
+        } // polymorph
+
+        // Possibly a terrible situation
+        private void tankMode()
+        {
+            if (tankTimer == -1)
+            {
+                CharacterBody p = PlayerCharacterMasterController.instances[0].master.GetBody();
+                tankDamageBuff = p.baseDamage * 2;
+                tankMoveDebuff = p.baseMoveSpeed / 4;
+                tankArmorBuff = p.baseArmor * 2;
+                p.baseDamage += tankDamageBuff;
+                p.baseMoveSpeed -= tankMoveDebuff;
+                p.baseArmor += tankArmorBuff;
+                tankTimer = 600; // 10 seconds
+            } // if
+        } // tankMode
+
+        // Give em a new nick
+        private void ruinName()
+        {
+            string[] goofNames = { "Jerry", "Sarah", "Commando", "Acrid", "Hopoo", "Ghor", "Paul", "Chris", "Providence",
+                "Jack-o-Lantern Dwyer", "Dwight", "Lucille Bluth", "John Mulaney", "Stone Titan",
+                "Leslie Knope", "Andy Dwyer", "April Ludgate", "Ben Wyatt", "Ann Perkins", "Chris Traeger", "Ron Swanson"};
+
+            goofName = goofNames[UnityEngine.Random.Range(0, goofNames.Length)];
+        } // void
+
+        // Not great to look at but seems optimized. Might loop this later for the eyes.
+        private void updateTimers()
+        {
+            if(dmgBuffTimer > 0)
+                dmgBuffTimer--;
+            if (dmgDebuffTimer > 0)
+                dmgDebuffTimer--;
+            if (moveBuffTimer > 0)
+                moveBuffTimer--;
+            if (hideTimer > 0)
+                hideTimer--;
+            if (trailTimer > 0)
+                trailTimer--;
+            if (hauntedTimer > 0)
+                hauntedTimer--;
+            if (funballTimer > 0)
+                funballTimer--;
+            if (ghostTimer > 0)
+                ghostTimer--;
+            if (tankTimer > 0)
+                tankTimer--;
+        } // timerHandler
+
+        // Hardcoded Finality
+        private void resolveTimers()
+        {
+            CharacterMaster master = PlayerCharacterMasterController.instances[0].master;
+
+            if (dmgBuffTimer == 0)
+            {
+                master.GetBody().baseDamage -= dmgBuff;
+                dmgBuffTimer = -1;
+            } // dmgBuffTimer
+
+            if (dmgDebuffTimer == 0)
+            {
+                master.GetBody().baseDamage += dmgDebuff;
+                dmgDebuffTimer = -1;
+            } // dmgDebuffTimer
+
+            if (hideTimer == 0)
+            {
+                master.GetBody().hideCrosshair = false;
+                hideTimer = -1;
+            } // hideTimer
+
+            if (trailTimer == 0)
+            {
+                for (int i = 0; i < trailArray.Length; i++)
+                {
+                    Destroy(trailArray[i].gameObject);
+                    trailArray[i] = null;
+                } // for
+                trailTimer = -1;
+            } // trailTimer
+
+            if (hauntedTimer == 0)
+            {
+                haunted = false;
+                hauntedTimer = -1;
+            } // hauntedTimer
+
+            if (funballTimer == 0)
+            {
+                Run.instance.enabledArtifacts.RemoveArtifact(ArtifactIndex.Bomb);
+                funballTimer = -1;
+            } // funballTimer
+
+            if (ghostTimer == 0)
+            {
+                master.inventory.RemoveItem(ItemIndex.Ghost, 1);
+                ghostTimer = -1;
+            } // ghostTimer
+
+            if (moveBuffTimer == 0)
+            {
+                master.GetBody().baseMoveSpeed -= moveBuff;
+                moveBuffTimer = -1;
+            } // moveBuffTimer
+
+            if (tankTimer == 0)
+            {
+                master.GetBody().baseDamage -= tankDamageBuff;
+                master.GetBody().baseMoveSpeed += tankMoveDebuff;
+                master.GetBody().baseArmor -= tankArmorBuff;
+                tankTimer = -1;
+            } // tankTimer
+        } // resolveTimers
+
+        public void Update()
+        {
+            // Run actually started
+            if (Run.instance.fixedTime > 0)
+            {
+                // Debugging Key
+                if (Input.GetKeyDown(KeyCode.F2))
+                {
+                    CharacterMaster master = PlayerCharacterMasterController.instances[0].master;
+
+                    //tankMode();
+
+                    MagicHandler testHandler = new MagicHandler(master);
+
+                    testHandler.Roll();
+
+                    // Testing
+                    //goGhost();
+                    //buffMove();
+                } // if
+
+                updateTimers();
+                resolveTimers();
+            } // if
+        } // Update
+    } // Chaos
+} // Chaos
